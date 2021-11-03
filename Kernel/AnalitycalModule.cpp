d6653d1a839ac0b70ebe030a79dfc544aa0e714c
Dima Truhsin <trushindima@yandex.ru>
2021-11-03 21:34:41 +0300

    Add preliminary version of Analytical module

--- Kernel/AnalitycalModule.cpp
+++ Kernel/AnalitycalModule.cpp
@@ -9,7 +9,13 @@ namespace NSKernel {
 CPlotData::CPlotData()
     : X_(kDefaultGridSize), Y0_(kDefaultGridSize, 0.),
       Y1_(kDefaultGridSize, 0.) {
-  fillXByDefault();
+  fillX();
+  assert(isCorrect_());
+}
+
+CPlotData::CPlotData(CIndex GridSize)
+    : X_(GridSize), Y0_(GridSize, 0.), Y1_(GridSize, 0.) {
+  fillX();
   assert(isCorrect_());
 }
 
@@ -18,24 +24,38 @@ CPlotData::CIndex CPlotData::gridSize() const {
   return X_.size();
 }
 
-const CPlotData::Real* CPlotData::dataX() const {
+const double* CPlotData::dataX() const {
   assert(isCorrect_());
   return X_.data();
 }
 
-const CPlotData::Real* CPlotData::dataY0() const {
+const double* CPlotData::dataY0() const {
   assert(isCorrect_());
   return Y0_.data();
 }
 
-const CPlotData::Real* CPlotData::dataY1() const {
+const double* CPlotData::dataY1() const {
   assert(isCorrect_());
   return Y1_.data();
 }
 
-void CPlotData::fillXByDefault() {
-  Real step = (Maximum_ - Minimum_) / (kDefaultGridSize - 1);
-  for (size_t i = 0; i < kDefaultGridSize; ++i) {
+void CPlotData::fillY0(const CContainer& Samples, const CDensity0& F) {
+  // Need to parallelize
+  for (size_t i = 0; i < X_.size(); ++i) {
+    Y0_[i] = F(Samples, X_[i]);
+  }
+}
+
+void CPlotData::fillY1(const CContainer& Samples, const CDensity1& F) {
+  // Need to parallelize
+  for (size_t i = 0; i < X_.size(); ++i) {
+    Y1_[i] = F(Samples, X_[i]);
+  }
+}
+
+void CPlotData::fillX() {
+  double step = (Maximum_ - Minimum_) / (X_.size() - 1);
+  for (size_t i = 0; i < X_.size(); ++i) {
     X_[i] = Minimum_ + step * i;
   }
 }
@@ -44,5 +64,36 @@ bool CPlotData::isCorrect_() const {
   return X_.size() == Y0_.size() && X_.size() == Y1_.size();
 }
 
+void CFunctionData::set(CContainer&& Samples) {
+  Samples_ = std::move(Samples);
+  PlotData_.fillY0(Samples_, Density0_);
+  PlotData_.fillY1(Samples_, Density1_);
+}
+
+const CPlotData& CFunctionData::plotData() const {
+  return PlotData_;
+}
+
+namespace NSAnalitycalModuleDetail {
+CAnalitycalModuleImpl::CAnalitycalModuleImpl()
+    : TextData_([this](const CTextData& Data) { handleTextData(Data); }),
+      DensityOut_([this]() -> CPlotDataGetType {
+        return std::cref(SpeedData_.plotData());
+      }) {
+}
+
+void CAnalitycalModuleImpl::handleTextData(const CTextData& Data) {
+  // preliminary implementation
+  CContainer Samples = getSpeedData(Data);
+  SpeedData_.set(std::move(Samples));
+  DensityOut_.notify();
+}
+
+CAnalitycalModuleImpl::CContainer
+CAnalitycalModuleImpl::getSpeedData(const CTextData& Data) const {
+  // TO DO
+  return CContainer();
+}
+} // namespace NSAnalitycalModuleDetail
 } // namespace NSKernel
 } // namespace NSApplication
--- Kernel/AnalitycalModule.h
+++ Kernel/AnalitycalModule.h
@@ -1,7 +1,10 @@
 #ifndef NSAPPLICATION_NSKERNEL_CANALITYCALMODULE_H
 #define NSAPPLICATION_NSKERNEL_CANALITYCALMODULE_H
 
+#include "Library/Observer/Observer.h"
 #include "Library/StlExtension/MvcWrappers.h"
+#include "MathFunction.h"
+#include "TextData.h"
 
 #include <vector>
 
@@ -9,49 +12,75 @@ namespace NSApplication {
 namespace NSKernel {
 
 class CPlotData {
-  using Real = double;
-  using Container = std::vector<Real>;
+  using CContainer = std::vector<double>;
 
 public:
   using CIndex = int64_t;
 
   CPlotData();
+  CPlotData(CIndex GridSize);
 
   CIndex gridSize() const;
 
-  const Real* dataX() const;
-  const Real* dataY0() const;
-  const Real* dataY1() const;
+  const double* dataX() const;
+  const double* dataY0() const;
+  const double* dataY1() const;
+
+  void fillY0(const CContainer& Samples, const CDensity0& F);
+  void fillY1(const CContainer& Samples, const CDensity1& F);
 
 private:
   static constexpr const CIndex kDefaultGridSize = 2500;
-  static constexpr const Real kMaximum = 2500.;
-  static constexpr const Real kMinimum = 1.;
+  static constexpr const double kMaximum = 2500.;
+  static constexpr const double kMinimum = 1.;
 
-  void fillXByDefault();
+  void fillX();
 
   bool isCorrect_() const;
 
-  Container X_;
-  Container Y0_;
-  Container Y1_;
-  Real Maximum_ = kMaximum;
-  Real Minimum_ = kMinimum;
+  CContainer X_;
+  CContainer Y0_;
+  CContainer Y1_;
+  double Maximum_ = kMaximum;
+  double Minimum_ = kMinimum;
 };
 
 class CFunctionData {
+  using CContainer = std::vector<double>;
+
 public:
+  void set(CContainer&& Samples);
+  const CPlotData& plotData() const;
+
 private:
-  // samples
-  // function
+  CContainer Samples_;
+  CDensity0 Density0_{};
+  CDensity1 Density1_{};
   CPlotData PlotData_;
 };
 
 namespace NSAnalitycalModuleDetail {
 class CAnalitycalModuleImpl {
+  using CContainer = std::vector<double>;
+
+  using CTextDataObserver = NSLibrary::CObserver<CTextData>;
+  using CTextDataInput = NSLibrary::CHotInput<CTextData>;
+
+  using CPlotDataObserver = NSLibrary::CObserver<CPlotData>;
+  using CPlotDataObservable = NSLibrary::CObservable<CPlotData>;
+  using CPlotDataGetType = CPlotDataObserver::CGetType;
+
 public:
+  CAnalitycalModuleImpl();
+
 private:
+  void handleTextData(const CTextData& Data);
+
+  CContainer getSpeedData(const CTextData& Data) const;
+
   CFunctionData SpeedData_;
+  CTextDataInput TextData_;
+  CPlotDataObservable DensityOut_;
 };
 
 } // namespace NSAnalitycalModuleDetail
