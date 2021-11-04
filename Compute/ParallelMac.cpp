b9738e8f77487847ada0a576a3c278e265a58d1b
Dima Truhsin <trushindima@yandex.ru>
2021-11-05 01:08:52 +0300

    Add Linux and Mac dummy implementation of Parallel Module

--- Compute/ParallelMac.cpp
+++ Compute/ParallelMac.cpp
@@ -1,11 +1,55 @@
 #include "ParallelMac.h"
+#include <cassert>
 
 namespace NSApplication {
 namespace NSCompute {
 
-CParallelMac::CParallelMac()
-{
+CParallelMac::CParallelMac(EParallelMode Mode) {
+  switch (Mode) {
+  case EParallelMode::Serial:
+    if (kIsSerialSupported)
+      Mode_ = Mode;
+    break;
+  case EParallelMode::Ppl:
+    if (kIsPplSupported)
+      Mode_ = Mode;
+    break;
+  case EParallelMode::Tbb:
+    if (kIsTbbSupported)
+      Mode_ = Mode;
+    break;
+  case EParallelMode::LibDispatch:
+    if (kIsLibDispatchSupported)
+      Mode_ = Mode;
+    break;
+  default:
+    assert(false);
+    break;
+  }
+}
 
+void CParallelMac::switchMode(EParallelMode Mode) {
+  switch (Mode) {
+  case EParallelMode::Serial:
+    if (kIsSerialSupported)
+      Mode_ = Mode;
+    break;
+  case EParallelMode::Ppl:
+    if (kIsPplSupported)
+      Mode_ = Mode;
+    break;
+  case EParallelMode::Tbb:
+    if (kIsTbbSupported)
+      Mode_ = Mode;
+    break;
+  case EParallelMode::LibDispatch:
+    if (kIsLibDispatchSupported)
+      Mode_ = Mode;
+    break;
+  default:
+    assert(false);
+    break;
+  }
 }
 
 } // namespace NSCompute
--- Compute/ParallelMac.h
+++ Compute/ParallelMac.h
@@ -1,14 +1,48 @@
 #ifndef NSAPPLICATION_NSCOMPUTE_CPARALLELMAC_H
 #define NSAPPLICATION_NSCOMPUTE_CPARALLELMAC_H
 
+#include "ParallelMode.h"
+#include "ParallelSerial.h"
 
 namespace NSApplication {
 namespace NSCompute {
 
-class CParallelMac
-{
+class CParallelMac {
 public:
-  CParallelMac();
+  CParallelMac(EParallelMode Mode);
+
+  template<class TIndex, class TFunction>
+  void for_(TIndex begin, TIndex end, const TFunction& F) {
+    switch (Mode_) {
+    case EParallelMode::Serial:
+      SerialComputer_.for_(begin, end, F);
+      break;
+    case EParallelMode::Ppl:
+      SerialComputer_.for_(begin, end, F);
+      break;
+    case EParallelMode::Tbb:
+      SerialComputer_.for_(begin, end, F);
+      break;
+    case EParallelMode::LibDispatch:
+      SerialComputer_.for_(begin, end, F);
+      break;
+    default:
+      break;
+    }
+  }
+
+  void switchMode(EParallelMode Mode);
+
+private:
+  static constexpr const bool kIsSerialSupported = true;
+  static constexpr const bool kIsPplSupported = false;
+  static constexpr const bool kIsTbbSupported = false;
+  static constexpr const bool kIsLibDispatchSupported = false;
+
+  static constexpr const EParallelMode kDefaultMode = EParallelMode::Serial;
+
+  CParallelSerial SerialComputer_;
+  EParallelMode Mode_ = kDefaultMode;
 };
 
 } // namespace NSCompute
