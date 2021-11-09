545f0cda41b88a976bcb5b17c43a385d4f97d914
Dima Truhsin <trushindima@yandex.ru>
2021-11-09 15:02:44 +0300

    Add call to no_subnormals() to speed up computations

--- SimdDetector.cpp
+++ SimdDetector.cpp
@@ -1,9 +1,11 @@
 #include "SimdDetector.h"
 #include "3dparty/vectorclass/instrset.h"
+#include "3dparty/vectorclass/vectorclass.h"
 
 namespace NSApplication {
 
 CSimdDetector::CSimdDetector() : InstructionLevel_(instrset_detect()) {
+  no_subnormals();
 }
 
 int CSimdDetector::level() const {
