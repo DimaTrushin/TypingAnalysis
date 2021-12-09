e304ec21e36b8bc154e5685d8c97be32c3bbe130
Dima Truhsin <trushindima@yandex.ru>
2021-12-09 21:14:47 +0300

    Refactor CApplicationGUI

--- ApplicationGUI.h
+++ ApplicationGUI.h
@@ -18,6 +18,13 @@
 namespace NSApplication {
 
 class CApplicationGUI {
+  using CSeanceView = NSInterface::CSeanceView;
+  using CTextModeView = NSInterface::CTextModeView;
+  using CTextPrinter = NSInterface::CTextPrinter;
+  using CPlotter = NSInterface::CPlotter;
+  using CStatisticsView = NSInterface::CStatisticsView;
+  using CKeySchemePlotter = NSInterface::CKeySchemePlotter;
+
 public:
   CApplicationGUI();
 
@@ -28,12 +35,12 @@ private:
   NSQt::CMainWindow AppWindow_;
 
 protected:
-  NSInterface::CSeanceView MainSeanceView_;
-  NSInterface::CTextModeView TextModeView_;
-  NSInterface::CTextPrinter MainTextPrinter_;
-  NSInterface::CPlotter SpeedPlotter_;
-  NSInterface::CStatisticsView StatisticsView_;
-  NSInterface::CKeySchemePlotter KeySchemePlotter_;
+  CSeanceView MainSeanceView_;
+  CTextModeView TextModeView_;
+  CTextPrinter MainTextPrinter_;
+  CPlotter SpeedPlotter_;
+  CStatisticsView StatisticsView_;
+  CKeySchemePlotter KeySchemePlotter_;
 
 #ifdef KEYBOARD_HANDLER_DEBUG
   NSAppDebug::CKeyboardHandlerDebugOut KeyboardHandlerOut_ = mainWindow();
